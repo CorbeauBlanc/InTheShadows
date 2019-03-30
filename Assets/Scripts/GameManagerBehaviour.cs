53a8d4000ffb9df0dae828424b1ddeb40e006bd5
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-03-30 21:34:28 +0100

    update animations

---
+++ Assets/Scripts/GameManagerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerBehaviour : MonoBehaviour
+{
+
+	public GameObject PointLight;
+	public GameObject SpotLight;
+
+	private Animator cameraAnimator;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		cameraAnimator = Camera.main.GetComponent<Animator>();
+	}
+
+}
