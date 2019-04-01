ee305ee86c5e148e3d4e813d9836bef2cad404be
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-04-01 16:34:56 +0200

    udpate anim

--- Assets/Scripts/GameManagerBehaviour.cs
+++ Assets/Scripts/GameManagerBehaviour.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class GameManagerBehaviour : MonoBehaviour
 {
 
-	public GameObject PointLight;
-	public GameObject SpotLight;
+	public static GameManagerBehaviour instance { get; private set; }
+
+	[HideInInspector] public bool UIMode = true;
+	[HideInInspector] public Animator selectedUIAnimator = null;
+	[HideInInspector] public GameObject selectedObject = null;
+	public GameObject pointLight;
+	public GameObject spotLight;
 
 	private Animator cameraAnimator;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		instance = this;
+	}
 
 	/// <summary>
 	/// Start is called on the frame when a script is enabled just before
@@ -17,6 +30,35 @@ public class GameManagerBehaviour : MonoBehaviour
 	void Start()
 	{
 		cameraAnimator = Camera.main.GetComponent<Animator>();
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if (cameraAnimator.GetCurrentAnimatorStateInfo(0).IsName("Introduction"))
+			return;
+		else if (!Cursor.visible)
+		{
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		if (UIMode)
+		{
+			if (Input.GetButtonDown("Fire1") && selectedObject)
+			{
+				Instantiate(selectedObject);
+				cameraAnimator.SetTrigger("look at fabric");
+				selectedUIAnimator.SetBool("idle", false);
+				selectedUIAnimator.SetTrigger("disappear");
+			}
+		}
+
+
 	}
 
 }
---
+++ Assets/Scripts/SelectionObjectBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionObjectBehaviour : MonoBehaviour
+{
+
+	public GameObject relatedIlluminatedObject;
+
+	private List<Material> objectsMaterial = new List<Material>();
+	private Animator objectAnimator;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		Renderer[] listRend = GetComponentsInChildren<Renderer>();
+		foreach (Renderer rnd in listRend)
+			objectsMaterial.Add(rnd.material);
+		objectAnimator = GetComponent<Animator>();
+	}
+
+	/// <summary>
+	/// Called every frame while the mouse is over the GUIElement or Collider.
+	/// </summary>
+	void OnMouseEnter()
+	{
+		if (GameManagerBehaviour.instance.UIMode)
+		{
+			GameManagerBehaviour.instance.selectedObject = relatedIlluminatedObject;
+			GameManagerBehaviour.instance.selectedUIAnimator = objectAnimator;
+			foreach (Material mat in objectsMaterial)
+				mat.EnableKeyword("_EMISSION");
+		}
+	}
+
+	/// <summary>
+	/// Called when the mouse is not any longer over the GUIElement or Collider.
+	/// </summary>
+	void OnMouseExit()
+	{
+		if (GameManagerBehaviour.instance.UIMode)
+		{
+			GameManagerBehaviour.instance.selectedObject = null;
+			GameManagerBehaviour.instance.selectedUIAnimator = null;
+			foreach (Material mat in objectsMaterial)
+				mat.DisableKeyword("_EMISSION");
+		}
+	}
+
+}
